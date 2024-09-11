a66eb03c8cd57e9367bfc668f3e0a8606fbdfe8e
Damien FAY <Damien.fay@etudiant.univ-reims.fr>
2024-09-11 16:24:32 +0200

    Pièges ajoutés

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -19,7 +20,13 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.stop()
+	if direction == 1 :
+		$AnimatedSprite2D.flip_h = true;
+	else :
+		$AnimatedSprite2D.flip_h = false;
 
 	move_and_slide()
