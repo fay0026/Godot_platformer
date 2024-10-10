fa19992e9c09a0edee7b3bede06f463d8a7c352f
Damien FAY <Damien.fay@etudiant.univ-reims.fr>
2024-10-10 09:01:04 +0200

    Ajout de l'écran de fin de partie

--- scenes/scripts/game_over.gd
+++ scenes/scripts/game_over.gd
@@ -13,3 +13,6 @@ func _on_restart_pressed() -> void:
 
 func _on_give_up_pressed() -> void:
 	get_tree().quit()
+
+func _on_personnage_game_over() -> void:
+	visible = true
--- scenes/scripts/gui.gd
+++ scenes/scripts/gui.gd
@@ -3,8 +3,10 @@ extends CanvasLayer
 @onready var lives: Label = $Control/BoxContainer3/Label
 @onready var diamonds: Label = $Control/BoxContainer/Label
 @onready var health: TextureProgressBar = $Control/hp_bar/TextureProgressBar
+@onready var score: Label = $Control/BoxContainer2/LabelScore
 
 var diamonds_total = 0
+var score_var = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +17,9 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_personnage_health_changed(HP) -> void:
+	score_var -= 15
 	health.value = HP
+	score.text = str(score_var)
 
 func _on_personnage_lost_life(lives_left) -> void:
 	health.value = 9
@@ -23,4 +27,6 @@ func _on_personnage_lost_life(lives_left) -> void:
 	
 func _on_diamant_diamond(value) -> void:
 	diamonds_total += value
+	score_var += value*100
 	diamonds.text = str(diamonds_total)
+	score.text = str(score_var)
